bc4f5e6da48a21c50e048e099d5d6a296b685bcf
Riwan <coefficriwan@gmail.com>
2024-08-25 17:44:57 +0200

    feat: add lua definition for Vec3, Camera

--- definition.d.lua
+++ definition.d.lua
@@ -1,1 +1,16 @@
-declare my_table: {}
+declare class Vec3
+  x: number
+  y: number
+  z: number
+  function __add(self, other: Vec3): Vec3
+  function __sub(self, other: Vec3): Vec3
+end
+
+declare Vec3: {
+  new: (x: number, y: number, z: number) -> Vec3,
+}
+
+declare class Camera
+	pos: Vec3
+	fovy: number
+end
